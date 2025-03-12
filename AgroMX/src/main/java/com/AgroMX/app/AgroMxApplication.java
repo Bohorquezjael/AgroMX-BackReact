920b247559601ed12dafedb2a45a828ba46a129c
Lisset Sánchez Juárez <131899300+Lisset100@users.noreply.github.com>
2025-03-12 17:28:02 -0600

    Proyecto-AgroMX
    Base de dato de AgroMX

---
+++ AgroMX/src/main/java/com/AgroMX/app/AgroMxApplication.java
@@ -0,0 +1,13 @@
+package com.AgroMX.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroMxApplication.class, args);
+	}
+
+}
---
+++ AgroMX/src/test/java/com/AgroMX/app/AgroMxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgroMX.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroMxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
