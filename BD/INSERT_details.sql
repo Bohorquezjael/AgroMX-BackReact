b2da31f87b539cafbdc14df396ab31ecd3e3d264
Thania Arredondo <elizabethania.te@gmail.com>
2025-03-11 19:55:40 -0600

    nuevo diagrama de entidad relación

--- BD/INSERT_details.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO details(information, fk_producer_has_technique_id)
-VALUES
-("Producida en la Ciudad de México", 1),
-("Producida en el Estado de México", 4),
-("Producida en la Ciudad de México", 5),
-("Producida en la Ciudad de México", 8);
--- BD/INSERT_producers_has_tecniques.sql
+++ BD/INSERT_producers_has_tecniques.sql
@@ -1,12 +1,14 @@
-INSERT INTO producers_has_tecniques(fk_producer_id, fk_tecnique_id)
+INSERT INTO producers_has_tecniques(producer_has_technique_id, fk_producer_id, fk_tecnique_id)
 VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4),
-(3, 2),
-(3, 4),
-(4, 3),
-(4, 5),
-(5, 1),
-(5, 5);
+(1, 1, 1),
+(1, 1, 2),
+(2, 2, 3),
+(2, 2, 4),
+(3, 3, 2),
+(3, 3, 4),
+(4, 4, 3),
+(4, 4, 5),
+(5, 5, 1),
+(5, 5, 5);
+
+SELECT * FROM producers_has_tecniques;
