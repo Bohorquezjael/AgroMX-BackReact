83ea8e9e26519548773a82e2833d5fed7ce6ce6c
Thania Arredondo <elizabethania.te@gmail.com>
2025-03-12 06:16:02 -0600

    script de user_has_privilege

--- BD/INSERT_orders.sql
+++ BD/INSERT_orders.sql
@@ -1,2 +1,2 @@
 INSERT INTO orders(date_time, fk_user_id)
-VALUE(2024-05-20, 1);
+VALUE("2024-05-20 18:00:19", 1);
---
+++ BD/INSERT_user_has_privilege.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_has_privilege(fk_privilage_id, fk_user_id)
+VALUES
+(1,1),
+(2,1);
