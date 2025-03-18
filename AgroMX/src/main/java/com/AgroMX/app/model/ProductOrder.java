c45b5a6869afc5c02f2a19e6a95c20b4c78e7f48
Jael Bohorquez <Jaelbkz@icloud.com>
2025-03-18 17:14:39 -0600

    cambie la relacion en order y product

--- AgroMX/src/main/java/com/AgroMX/app/model/ProductOrder.java
+++ AgroMX/src/main/java/com/AgroMX/app/model/ProductOrder.java
@@ -1,16 +1,7 @@
 package com.AgroMX.app.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "ProductOrder")
@@ -24,8 +15,7 @@ public class ProductOrder {
     @Column(name = "product_order_id", nullable = false)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    
     private Product productId;
 
     @ManyToOne
