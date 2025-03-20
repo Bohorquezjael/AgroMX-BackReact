3ad7d2109a1aaf34bfa6c9cfa86488caa1d6ef42
Jael Bohorquez <Jaelbkz@icloud.com>
2025-03-19 18:56:16 -0600

    fix: lista completa de productos

--- AgroMX/src/main/java/com/AgroMX/app/controller/impl/ProductControllerImp.java
+++ AgroMX/src/main/java/com/AgroMX/app/controller/impl/ProductControllerImp.java
@@ -1,16 +1,9 @@
 package com.AgroMX.app.controller.impl;
 
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.AgroMX.app.controller.ProductController;
 import com.AgroMX.app.model.Product;
@@ -34,7 +27,7 @@ public class ProductControllerImp implements ProductController{
                 .orElseGet(() -> ResponseEntity.status(400).build());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     @Override
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         return productService.getProduct(id)
@@ -59,15 +52,15 @@ public class ProductControllerImp implements ProductController{
 
     @GetMapping("/category/{id}")
     @Override
-    public ResponseEntity<Set<Product>> getProductByCategory(Long id) {
+    public ResponseEntity<Iterable<Product>> getProductByCategory(Long id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getProductByCategory'");
     }
 
     @GetMapping("/all")
     @Override
-    public ResponseEntity<Set<Product>> getAllProducts() {
-        Set<Product> products = (Set<Product>) productService.getAllProducts();
+    public ResponseEntity<Iterable<Product>> getAllProducts() {
+        List<Product> products = (List<Product>) productService.getAllProducts();
         if (products.isEmpty()) {
             return ResponseEntity.status(404).build();
         } else {
